24ada3c9373bff09d7fc0a1a94ec340dcf57001f
kecorbin <kecorbin@cisco.com>
2018-01-18 10:24:15 -0600

    change repo location in setup scripts

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 sudo echo "pulling docker images" >> /home/cisco/docker.log
-sudo docker-compose -f /home/cisco/netdevops-metrics/docker-compose.yaml pull
+sudo docker-compose -f /home/cisco/metrics/docker-compose.yaml pull
 sudo echo "starting containers" >> /home/cisco/docker.log
-sudo docker-compose -f /home/cisco/netdevops-metrics/docker-compose.yaml up -d
+sudo docker-compose -f /home/cisco/metrics/docker-compose.yaml up -d
